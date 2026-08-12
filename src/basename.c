a1c78d12c397af053dcef03df4fed074577afd76
littlefly365 <c081526@hotmail.com>
2026-08-12 14:29:05 -0500

    basename.c: clean the code using check macro instead of (argc == 1) ? op1 : op2

--- src/basename.c
+++ src/basename.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include "libbox.h"
 
+#define check(op1, op2) ((argc == 1) ? op1 : op2)
+
 int basename_main(int argc, char *argv[])
 {
-	fprintf((argc == 1) ? stdout : stderr, (argc == 1) ? basename(argv[0]): "usage: basename <string>");
-	fputc('\n', (argc == 1) ? stdout : stderr);
-	return (argc == 1) ? 0 : 1;
+	fputs(check(basename(argv[0]), "usage: basename <string>"), check(stdout, stderr));
+	fputc('\n', check(stdout, stderr));
+	return check(0, 1);
 }
